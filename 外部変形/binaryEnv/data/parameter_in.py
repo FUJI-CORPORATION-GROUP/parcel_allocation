bf57bd0c733d68fc6287ab6754d2aa31c46bed2a
Taga <tagahei0505@gmail.com>
2024-04-10 22:56:49 +0900

    ref: 変数名変更 受け取り側の処理と入力側の処理の変数名の違いを修正

--- 外部変形/binaryEnv/data/parameter_in.py
+++ 外部変形/binaryEnv/data/parameter_in.py
@@ -11,8 +11,8 @@ info = codecs.open('data/information.txt','w',"shift-jis")
 
 #コマンドライン引数（最小目標面積，最大目標面積）
 road_width=4000
-goal_min_area = 100000000
-goal_max_area = 120000000
+target_min_area = 100000000
+target_max_area = 120000000
 # maguti = 7600
 # goal_area = 100000000
 
@@ -23,19 +23,19 @@ for i in range(2,len(sys.argv)):
     road_width=float(sys.argv[i][2:len(sys.argv[i])]) * 1000
   #bから始まる情報はmaguti(間口)に代入
   elif re.match(r"/b",sys.argv[i]):
-    goal_min_area=float(sys.argv[i][2:len(sys.argv[i])]) * 1000000
+    target_min_area=float(sys.argv[i][2:len(sys.argv[i])]) * 1000000
   #cから始まる情報はgoal_area(目標面積)に代入
   elif re.match(r"/c",sys.argv[i]):
-    goal_max_area=float(sys.argv[i][2:len(sys.argv[i])]) * 1000000
+    target_max_area=float(sys.argv[i][2:len(sys.argv[i])]) * 1000000
 
 print(sys.argv)
 print(" ")
-print("goal_min_area:" + str(goal_min_area))
-print("goal_max_area:" + str(goal_max_area))
+print("target_min_area:" + str(target_min_area))
+print("target_max_area:" + str(target_max_area))
 print(" ")
 
-info.write(str(goal_min_area) + "\n")
-info.write(str(goal_max_area) + "\n")
+info.write(str(target_min_area) + "\n")
+info.write(str(target_max_area) + "\n")
 
 #コマンドライン引数(temp.txt)の受け取り
 file = sys.argv[1]
@@ -45,7 +45,7 @@ f=open(file,mode="r")
 
 #変数を宣言（接道辺，道作成辺，街区，座標，選択された連続線，枠上判定）
 make_road_edge = []
-road_frame = []
+road_edge_point_list = []
 xy = []
 
 #tempの中身を取得
@@ -53,27 +53,27 @@ for line in f:
   if re.match(r"hp",line):
     #一行ごとに読み込み
     xy=line.split()
-    road_frame.append([float(xy[1]), float(xy[2])])
-for i in range(len(road_frame)):
-  for j in range(1, len(road_frame[i])):
-    print(" " + str(road_frame[i][j - 1]) + " " + str(road_frame[i][j]))
+    road_edge_point_list.append([float(xy[1]), float(xy[2])])
+for i in range(len(road_edge_point_list)):
+  for j in range(1, len(road_edge_point_list[i])):
+    print(" " + str(road_edge_point_list[i][j - 1]) + " " + str(road_edge_point_list[i][j]))
 f.close()
 
 # 道路と設置している辺の座標の取得
-for i in range(len(road_frame)):
-  if i == len(road_frame) - 1:
-    info.write(str(road_frame[i][0]) + " " + str(road_frame[i][1]) + "\n")
+for i in range(len(road_edge_point_list)):
+  if i == len(road_edge_point_list) - 1:
+    info.write(str(road_edge_point_list[i][0]) + " " + str(road_edge_point_list[i][1]) + "\n")
   else:
-    info.write(str(road_frame[i][0]) + " " + str(road_frame[i][1]) + " ")
+    info.write(str(road_edge_point_list[i][0]) + " " + str(road_edge_point_list[i][1]) + " ")
 
 # 道路情報をjsonファイルに書き込む
-def input_to_json(road_width, goal_min_area, goal_max_area, road_frame):
+def input_to_json(road_width, target_min_area, target_max_area, road_edge_point_list):
   # TODO: のちのちInputの構造体を作るとよりわかりやすい
-  input_data = {'road_width': road_width, 'goal_min_area': goal_min_area, 'goal_max_area': goal_max_area, 'road_frame': road_frame}
+  input_data = {'road_width': road_width, 'target_min_area': target_min_area, 'target_max_area': target_max_area, 'road_edge_point_list': road_edge_point_list}
   
   input_json_data = json.dumps(input_data, indent=4)
   
   with open('data/road_input_data.json', 'w') as f:
     f.write(input_json_data)
 
-input_to_json(road_width, goal_min_area, goal_max_area, road_frame)
+input_to_json(road_width, target_min_area, target_max_area, road_edge_point_list)
