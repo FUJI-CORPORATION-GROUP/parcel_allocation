0e49b86bf6819f892167c67c104009743d70b0a9
Taga <tagahei0505@gmail.com>
2024-04-10 23:19:00 +0900

    add: TXTでの記録を削除

--- 外部変形/binaryEnv/data/parameter_in.py
+++ 外部変形/binaryEnv/data/parameter_in.py
@@ -4,17 +4,13 @@ import re
 import json
 
 print("----parameter_in.py----")
-# 道路情報を入力している？
-
-#書き込み用のファイルを展開
-info = codecs.open('data/information.txt','w',"shift-jis")
+# 道路情報を入力している
+# TODO: parameter_in.pyとun_road_make.pyの役割がファイル名でわかりにくいため，統合 or リネームする
 
 #コマンドライン引数（最小目標面積，最大目標面積）
 road_width=4000
 target_min_area = 100000000
 target_max_area = 120000000
-# maguti = 7600
-# goal_area = 100000000
 
 # コマンドライン引数の二つ目から読み込み
 for i in range(2,len(sys.argv)):
@@ -34,21 +30,14 @@ print("target_min_area:" + str(target_min_area))
 print("target_max_area:" + str(target_max_area))
 print(" ")
 
-info.write(str(target_min_area) + "\n")
-info.write(str(target_max_area) + "\n")
 
-#コマンドライン引数(temp.txt)の受け取り
-file = sys.argv[1]
-
-#読み取り専用で代入
-f=open(file,mode="r")
-
-#変数を宣言（接道辺，道作成辺，街区，座標，選択された連続線，枠上判定）
-make_road_edge = []
+# 道路の座標を取得
 road_edge_point_list = []
 xy = []
 
 #tempの中身を取得
+tmp_file = sys.argv[1]
+f=open(tmp_file,mode="r")
 for line in f:
   if re.match(r"hp",line):
     #一行ごとに読み込み
@@ -58,13 +47,6 @@ for i in range(len(road_edge_point_list)):
   for j in range(1, len(road_edge_point_list[i])):
     print(" " + str(road_edge_point_list[i][j - 1]) + " " + str(road_edge_point_list[i][j]))
 f.close()
-
-# 道路と設置している辺の座標の取得
-for i in range(len(road_edge_point_list)):
-  if i == len(road_edge_point_list) - 1:
-    info.write(str(road_edge_point_list[i][0]) + " " + str(road_edge_point_list[i][1]) + "\n")
-  else:
-    info.write(str(road_edge_point_list[i][0]) + " " + str(road_edge_point_list[i][1]) + " ")
 
 # 道路情報をjsonファイルに書き込む
 def input_to_json(road_width, target_min_area, target_max_area, road_edge_point_list):
