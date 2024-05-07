235b977da744d51a0a73f7361c0b755729e379d9
Taga <tagahei0505@gmail.com>
2024-05-07 21:30:47 +0900

    update: 不要なimportと出力先ファイルの変更
    point,frameなどでdraw_dxfを呼び出そうとすると，循環参照になっていたが，そもそもdraw_dxfでpoint,frameなどを使っていなかった．
    また，出力先を指定しやすいように変数に格納した

--- 外部変形/binaryEnv/draw_dxf.py
+++ 外部変形/binaryEnv/draw_dxf.py
@@ -1,8 +1,9 @@
 import ezdxf
 import numpy as np
-from point import Point
-from frame import Frame
-from plan import Plan
+
+# 出力先DXFファイル
+output_file_name = "output.dxf"
+output_file_path = "./out/" + output_file_name
 
 
 def draw_line_by_point(point_list):
@@ -11,63 +12,62 @@ def draw_line_by_point(point_list):
         point_list (list): Point型のリスト
     """
 
-    # clear_dxf()
-    
-    doc = ezdxf.readfile(r"./output.dxf")
+    doc = ezdxf.readfile(output_file_path)
 
-    #モデル空間に新しいエンティティを作成
+    # モデル空間に新しいエンティティを作成
     msp = doc.modelspace()
     for i in range(len(point_list)):
         s = point_list[i].to_np_array()
-        e = point_list[i+1].to_np_array() if i+1 < len(point_list) else point_list[0].to_np_array()
+        e = point_list[i+1].to_np_array() if i + \
+            1 < len(point_list) else point_list[0].to_np_array()
         msp.add_line(s, e)
 
-    doc.saveas('output.dxf')
+    doc.saveas(output_file_path)
 
 # Point型
-def draw_line_by_point_color(point_list,color_index):
+def draw_line_by_point_color(point_list, color_index):
     """Point型のリストと色指定してdxf出力
 
     Args:
         point_list (list): Point型のリスト
         color_index (int): 色
     """
-    
-    # clear_dxf()
-    
-    doc = ezdxf.readfile(r"./output.dxf")
 
-    #モデル空間に新しいエンティティを作成
+    doc = ezdxf.readfile(output_file_path)
+
+    # モデル空間に新しいエンティティを作成
     msp = doc.modelspace()
     for i in range(len(point_list)):
         s = point_list[i].to_np_array()
-        e = point_list[i+1].to_np_array() if i+1 < len(point_list) else point_list[0].to_np_array()
-        msp.add_line(s, e,dxfattribs={'color':color_index})
+        e = point_list[i+1].to_np_array() if i + \
+            1 < len(point_list) else point_list[0].to_np_array()
+        msp.add_line(s, e, dxfattribs={'color': color_index})
 
-    doc.saveas('output.dxf')
+    doc.saveas(output_file_path)
 
 # FrameList型
-def draw_line_by_frame_list_color(frame_list,color_index):
+def draw_line_by_frame_list_color(frame_list, color_index):
     """FrameList型のリストと色指定してdxf出力
 
     Args:
         frame_list (list): FrameList型のリスト
         color_index (int): 色
     """
-    # clear_dxf()
-    
-    doc = ezdxf.readfile(r"./output.dxf")
+
+    doc = ezdxf.readfile(output_file_path)
     msp = doc.modelspace()
 
     for i in range(len(frame_list)):
-        #モデル空間に新しいエンティティを作成
+        # モデル空間に新しいエンティティを作成
         point_list = frame_list[i].points
         for i in range(len(point_list)):
             s = point_list[i].to_np_array()
-            e = point_list[i+1].to_np_array() if i+1 < len(point_list) else point_list[0].to_np_array()
-            msp.add_line(s, e,dxfattribs={'color':color_index})
+            e = point_list[i+1].to_np_array() if i + \
+                1 < len(point_list) else point_list[0].to_np_array()
+            msp.add_line(s, e, dxfattribs={'color': color_index})
 
-    doc.saveas('output.dxf')
+    doc.saveas(output_file_path)
+
 
 def draw_dxf_by_plan_list(plan_list, color_index):
     """PlanList型のリストと色指定してdxf出力
@@ -76,24 +76,24 @@ def draw_dxf_by_plan_list(plan_list, color_index):
         plan_list (list): PlanList型のリスト
         color_index (int): 色
     """
-    doc = ezdxf.readfile(r"./output.dxf")
+    doc = ezdxf.readfile(output_file_path)
     msp = doc.modelspace()
     for plan in plan_list:
-        frame_list = plan.get_frame_list()        
+        frame_list = plan.get_frame_list()
         for i in range(len(frame_list)):
-            #モデル空間に新しいエンティティを作成
+            # モデル空間に新しいエンティティを作成
             point_list = frame_list[i].points
             for i in range(len(point_list)):
                 s = point_list[i].to_np_array()
-                e = point_list[i+1].to_np_array() if i+1 < len(point_list) else point_list[0].to_np_array()
-                msp.add_line(s, e,dxfattribs={'color':color_index})
-    
-    doc.saveas('output.dxf')
+                e = point_list[i+1].to_np_array() if i + \
+                    1 < len(point_list) else point_list[0].to_np_array()
+                msp.add_line(s, e, dxfattribs={'color': color_index})
+
+    doc.saveas(output_file_path)
 
 
 def clear_dxf():
     """dxfファイルをクリアする
     """
     doc = ezdxf.new("R2010")
-    doc.saveas('output.dxf')
-
+    doc.saveas(output_file_path)
