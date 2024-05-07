9dffed1ada5c85b94e395d2c448f747cf2589741
Taga <tagahei0505@gmail.com>
2024-05-08 08:13:12 +0900

    update: 描写処理の関数化
    同じ処理をたくさん書いてたため，各処理を書きだした

--- 外部変形/binaryEnv/draw_dxf.py
+++ 外部変形/binaryEnv/draw_dxf.py
@@ -11,16 +11,10 @@ def draw_line_by_point(point_list):
     Args:
         point_list (list): Point型のリスト
     """
-
+    dxfattribs = {'color': 7}
     doc = ezdxf.readfile(output_file_path)
-
-    # モデル空間に新しいエンティティを作成
     msp = doc.modelspace()
-    for i in range(len(point_list)):
-        s = point_list[i].to_np_array()
-        e = point_list[i+1].to_np_array() if i + \
-            1 < len(point_list) else point_list[0].to_np_array()
-        msp.add_line(s, e)
+    draw_line_list(msp, point_list,dxfattribs)
 
     doc.saveas(output_file_path)
 
@@ -32,17 +26,10 @@ def draw_line_by_point_color(point_list, color_index):
         point_list (list): Point型のリスト
         color_index (int): 色
     """
-
+    dxfattribs = {'color': color_index}
     doc = ezdxf.readfile(output_file_path)
-
-    # モデル空間に新しいエンティティを作成
     msp = doc.modelspace()
-    for i in range(len(point_list)):
-        s = point_list[i].to_np_array()
-        e = point_list[i+1].to_np_array() if i + \
-            1 < len(point_list) else point_list[0].to_np_array()
-        msp.add_line(s, e, dxfattribs={'color': color_index})
-
+    draw_line_list(msp, point_list, dxfattribs)
     doc.saveas(output_file_path)
 
 # FrameList型
@@ -53,18 +40,12 @@ def draw_line_by_frame_list_color(frame_list, color_index):
         frame_list (list): FrameList型のリスト
         color_index (int): 色
     """
-
+    dxfattribs = {'color': color_index}
     doc = ezdxf.readfile(output_file_path)
     msp = doc.modelspace()
-
     for i in range(len(frame_list)):
-        # モデル空間に新しいエンティティを作成
         point_list = frame_list[i].points
-        for i in range(len(point_list)):
-            s = point_list[i].to_np_array()
-            e = point_list[i+1].to_np_array() if i + \
-                1 < len(point_list) else point_list[0].to_np_array()
-            msp.add_line(s, e, dxfattribs={'color': color_index})
+        draw_line_list(msp, point_list, dxfattribs)
 
     doc.saveas(output_file_path)
 
@@ -78,16 +59,15 @@ def draw_dxf_by_plan_list(plan_list, color_index):
     """
     doc = ezdxf.readfile(output_file_path)
     msp = doc.modelspace()
+    
+    dxfattribs = {'color': color_index}
+
     for plan in plan_list:
         frame_list = plan.get_frame_list()
         for i in range(len(frame_list)):
             # モデル空間に新しいエンティティを作成
             point_list = frame_list[i].points
-            for i in range(len(point_list)):
-                s = point_list[i].to_np_array()
-                e = point_list[i+1].to_np_array() if i + \
-                    1 < len(point_list) else point_list[0].to_np_array()
-                msp.add_line(s, e, dxfattribs={'color': color_index})
+            draw_line_list(msp, point_list, dxfattribs)
 
     doc.saveas(output_file_path)
 
@@ -97,3 +77,28 @@ def clear_dxf():
     """
     doc = ezdxf.new("R2010")
     doc.saveas(output_file_path)
+
+
+def draw_line_list(msp, point_list, dxfattribs):
+    """Draw a line from point_list.
+
+    Args:
+        msp (Modelspace): dxfのモデル空間
+        point_list ([Point]): Point型のリスト
+        dxfattribs (Object): dxfの属性
+    """
+    for i in range(len(point_list)):
+        start_point = point_list[i].to_np_array()
+        end_point = point_list[(i+1)%len(point_list)].to_np_array()
+        msp.add_line(start_point, end_point, dxfattribs=dxfattribs)
+
+def draw_line(msp, start, end, dxfattribs):
+    """Draw a line from start to end.
+
+    Args:
+        msp (Modelspace): dxfのモデル空間
+        start (Point): 始点
+        end (Point): 終点
+        dxfattribs (Object): dxfの属性
+    """
+    msp.add_line(start.to_np_array(), end.to_np_array(), dxfattribs=dxfattribs)
