d3904ce1585616bf9109f832f6d3a515fa971a8c
Taga <tagahei0505@gmail.com>
2024-05-11 16:12:08 +0900

    add: 最大・最小間口の入力機構
    目標面積と同様に道路入力に組み込んだけど，ここでいいんかなという不安はあります．
    ただ入力に関しては大きく変わりそうなのでとりあえず入力出来たら良いかと思ってます．

--- 外部変形/binaryEnv/data/parameter_in.py
+++ 外部変形/binaryEnv/data/parameter_in.py
@@ -10,22 +10,32 @@ print("----parameter_in.py----")
 road_width = 4000
 target_min_area = 100000000
 target_max_area = 120000000
+min_maguchi = 7000
+max_maguchi = 8000
 
 # コマンドライン引数の二つ目から読み込み
-for i in range(2, len(sys.argv)):
-    data = sys.argv[i]
-    # road_width(道幅)に代入
-    if re.match(r"/road_width", data):
-        road_width = float(data.split(":")[1]) * 1000
-    # 最小目標面積
-    elif re.match(r"/target_min_area", data):
-        target_min_area = float(data.split(":")[1]) * 1000000
-    # 最大目標面積
-    elif re.match(r"/target_max_area", data):
-        target_max_area = float(data.split(":")[1]) * 1000000
+for i in range(2,len(sys.argv)):
+  data = sys.argv[i]
+  #road_width(道幅)に代入
+  if re.match(r"/road_width",data):
+    road_width=float(data.split(':')[1]) * 1000
+  # 最小間口 m -> mm
+  elif re.match(r"/min_maguchi",data):
+    min_maguchi=float(data.split(':')[1]) * 1000
+  # 最大間口 m -> mm
+  elif re.match(r"/max_maguchi",data):
+    max_maguchi=float(data.split(':')[1]) * 1000
+  # 最小目標面積 m^2 -> mm^2
+  elif re.match(r"/target_min_area",data):
+    target_min_area=float(data.split(':')[1]) * 1000000
+  # 最大目標面積 m^2 -> mm^2
+  elif re.match(r"/target_max_area",data):
+    target_max_area=float(data.split(':')[1]) * 1000000
 
 print(sys.argv)
 print(" ")
+print("min_maguchi:" + str(min_maguchi))
+print("max_maguchi:" + str(max_maguchi))
 print("target_min_area:" + str(target_min_area))
 print("target_max_area:" + str(target_max_area))
 print(" ")
@@ -56,18 +66,14 @@ f.close()
 
 # 道路情報をjsonファイルに書き込む
 def input_to_json(road_width, target_min_area, target_max_area, road_edge_point_list):
-    # TODO: のちのちInputの構造体を作るとよりわかりやすい
-    input_data = {
-        "road_width": road_width,
-        "target_min_area": target_min_area,
-        "target_max_area": target_max_area,
-        "road_edge_point_list": road_edge_point_list,
-    }
-
-    input_json_data = json.dumps(input_data, indent=4)
-
-    with open("data/road_input_data.json", "w") as f:
-        f.write(input_json_data)
-
+  # TODO: のちのちInputの構造体を作るとよりわかりやすい
+  input_data = {'road_width': road_width,
+                'min_maguchi': min_maguchi, 'max_maguchi': max_maguchi, 
+                'target_min_area': target_min_area, 'target_max_area': target_max_area, 'road_edge_point_list': road_edge_point_list}
+  
+  input_json_data = json.dumps(input_data, indent=4)
+  
+  with open('data/road_input_data.json', 'w') as f:
+    f.write(input_json_data)
 
 input_to_json(road_width, target_min_area, target_max_area, road_edge_point_list)
